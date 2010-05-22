2e60908e27fcc91ebc319c4e541369bdee120ec9
Adam Elliot <adam@adamelliot.com>
2010-05-22 15:36:27 -0600

    Stop deleting your work genious

--- lib/resource_mapper.rb
+++ lib/resource_mapper.rb
@@ -8,7 +8,24 @@ module ResourceMapper
   FAILABLE_ACTIONS  = (ACTIONS - [:index]).freeze
   NAME_ACCESSORS    = [:model_name, :route_name, :object_name]
   
-  autoload :Controller, 'resource_mapper/controller'
+  autoload :Accessors,              'resource_mapper/accessors'
+  autoload :ActionOptions,          'resource_mapper/action_options'
+  autoload :Actions,                'resource_mapper/actions'
+  autoload :ClassMethods,           'resource_mapper/class_methods'
+  autoload :Controller,             'resource_mapper/controller'
+  autoload :FailableActionOptions,  'resource_mapper/failable_action_options'
+  autoload :ResponseCollector,      'resource_mapper/response_collector'
+
+  module Helpers
+    autoload :Internal,       'resource_mapper/controller/internal'
+    autoload :Nested,         'resource_mapper/controller/nested'
+    autoload :CurrentObjects, 'resource_mapper/controller/current_objects'
+
+    include ResourceMapper::Helpers::Internal
+    include ResourceMapper::Helpers::Nested
+    include ResourceMapper::Helpers::CurrentObjects
+  end
+
 end
 
 require 'sinatra/resource'
